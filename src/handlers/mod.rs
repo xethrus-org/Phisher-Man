d0eab135679d8a568e56554b8dd66d093cc7b91a
JDisaster <fork868788@gmail.com>
2025-11-30 22:34:32 -0800

    feat(frontend) fix and create missing UI logic

---
+++ src/handlers/mod.rs
@@ -0,0 +1,11 @@
+pub mod campaigns;
+pub mod companies;
+pub mod email;
+pub mod employees;
+pub mod templates;
+
+pub use campaigns::*;
+pub use companies::*;
+pub use email::*;
+pub use employees::*;
+pub use templates::*;
